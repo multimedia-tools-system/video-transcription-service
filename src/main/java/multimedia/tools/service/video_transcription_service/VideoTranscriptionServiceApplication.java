4220e4f5bc49fd2349b67b83a95bfb263b23aede
espada1317 <trifan.denis1999@gmail.com>
2025-03-26 10:46:14 +0200

    Added initial configurations

---
+++ src/main/java/multimedia/tools/service/video_transcription_service/VideoTranscriptionServiceApplication.java
@@ -0,0 +1,13 @@
+package multimedia.tools.service.video_transcription_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VideoTranscriptionServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VideoTranscriptionServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/multimedia/tools/service/video_transcription_service/VideoTranscriptionServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package multimedia.tools.service.video_transcription_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VideoTranscriptionServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
