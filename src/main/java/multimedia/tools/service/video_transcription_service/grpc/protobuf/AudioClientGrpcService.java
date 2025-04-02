86639eb5f82313c0add96c0acd36f9c00811bb93
espada1317 <trifan.denis1999@gmail.com>
2025-04-02 15:57:55 +0300

    Added request - stream response grpc service for audio transcribe

--- src/main/java/multimedia/tools/service/video_transcription_service/grpc/protobuf/AudioClientGrpcService.java
+++ src/main/java/multimedia/tools/service/video_transcription_service/grpc/protobuf/AudioClientGrpcService.java
@@ -2,26 +2,33 @@ package multimedia.tools.service.video_transcription_service.grpc.protobuf;
 
 import io.grpc.ManagedChannel;
 import io.grpc.ManagedChannelBuilder;
+import io.grpc.stub.StreamObserver;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Component;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.CompletableFuture;
 
 @Component
 @RequiredArgsConstructor
 public class AudioClientGrpcService
 {
 
-    private final TranscribeSenderGrpc.TranscribeSenderBlockingStub transcribeSenderBlockingStub;
+    private final TranscribeSenderGrpc.TranscribeSenderStub asyncStub;
 
     public AudioClientGrpcService()
     {
         ManagedChannel managedChannel = ManagedChannelBuilder.forAddress("localhost", 50051)
                 .usePlaintext()
                 .build();
-        this.transcribeSenderBlockingStub = TranscribeSenderGrpc.newBlockingStub(managedChannel);
+        this.asyncStub = TranscribeSenderGrpc.newStub(managedChannel);
     }
 
     public String sendTranscribe(String model, String fileStoragePath, String operation)
     {
+        String result = "";
+
         AudioTranscribeService.RequestTranscribe requestTranscribe = AudioTranscribeService.RequestTranscribe
                 .newBuilder()
                 .setModel(model)
@@ -29,9 +36,45 @@ public class AudioClientGrpcService
                 .setOperation(operation)
                 .build();
 
-        AudioTranscribeService.ResponseTranscribe responseTranscribe = transcribeSenderBlockingStub.sendTranscribe(requestTranscribe);
+        CompletableFuture<List<AudioTranscribeService.ResponseTranscribe>> future = new CompletableFuture<>();
+        List<AudioTranscribeService.ResponseTranscribe> progressList = new ArrayList<>();
 
-        return responseTranscribe.getTranscribe();
+        asyncStub.sendTranscribe(requestTranscribe, new StreamObserver<>() {
+            @Override
+            public void onNext(AudioTranscribeService.ResponseTranscribe responseTranscribe)
+            {
+                System.out.println("Status: " + responseTranscribe.getStatus()
+                        + "; Progress: " + responseTranscribe.getProgress()
+                        + "; Transcribe: " + responseTranscribe.getTranscribe());
+                progressList.add(responseTranscribe);
+            }
+
+            @Override
+            public void onError(Throwable throwable)
+            {
+                System.err.println("Error: " + throwable.getMessage());
+                future.completeExceptionally(throwable);
+            }
+
+            @Override
+            public void onCompleted()
+            {
+                System.out.println("Parsing completed!");
+                future.complete(progressList);
+            }
+        });
+
+        try
+        {
+            List<AudioTranscribeService.ResponseTranscribe> finalResults = future.get();
+            System.out.println("Final progress list: " + finalResults);
+        }
+        catch (Exception e)
+        {
+            e.printStackTrace();
+        }
+
+        return result;
     }
 
 }
